a37bf09bc7bd8687fff1552c7120c920e11f3bf3
Leon Magnus <dalil.dig@gmail.com>
2017-10-24 22:52:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,17 +9,16 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
 	extended: true
 }));
-//recupe les donne
-const info = require("./info.json");
+const token=process.env.Facebook_TOKEN;
 //teste la connection avec la page facebook
-app.set('port', info.token);
+app.set('port',token);
 app.get('/', function (req, res) {
 	res.send('salut les amie');
 });
 //pour verifier
 app.get('/webhook/', function (req, res) {
-	if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+	if (req.query['hub.verify_token'] ===process.env.Verifier_jeton) {
 		res.send(req.query['hub.challenge'])
 	}
-	res.send('Error, wrong token')
+	res.send('Error, wrong token');
 })
