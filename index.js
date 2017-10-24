3cf5f8abd11f22a9590f2caad4f4572c22560c0b
Leon Magnus <dalil.dig@gmail.com>
2017-10-24 22:55:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get('/', function (req, res) {
 	res.send('salut les amie');
 });
 //pour verifier
-app.get('/webhook/', function (req, res) {
+app.get('/webhook', function (req, res) {
 	if (req.query['hub.verify_token'] ===process.env.Verifier_jeton) {
 		res.send(req.query['hub.challenge'])
 	}
