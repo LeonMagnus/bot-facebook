5eeafa8da2e4bda86264441db94239057e508529
Leon Magnus <dalil.dig@gmail.com>
2017-10-25 20:53:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,14 +12,14 @@ app.use(bodyParser.urlencoded({
 //teste la connection avec la page facebook
 app.set('port',process.env.PORT||5000);
 app.get('/', function (req, res) {
-	res.send('salut les amie');
+	res.send('pour le bot facebook');
 });
 //pour verifier
 app.get('/webhook', function (req, res) {
 	if (req.query['hub.verify_token'] ===process.env.Verifier_jeton) {
 		res.send(req.query['hub.challenge'])
 	}
-	res.send('Error, wrong token');
+	res.send('dsl, token faux');
 })
 app.listen(app.get('port'), function() {
 	console.log('running on port', app.get('port'))
